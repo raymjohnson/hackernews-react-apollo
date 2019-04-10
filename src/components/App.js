ec0b919e4ad50b478c7e54660dd1e1e2638b12ea
Rachel Johnson <rachel.johnson@cultureamp.com>
2019-04-10 15:41:03 +1000

    Create a mutation to create a link and render form

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import LinkList from "./LinkList";
+import CreateLink from "./CreateLink";
 
 export default class App extends Component {
   render() {
-    return <LinkList />;
+    return <CreateLink />;
   }
 }
